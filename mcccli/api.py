fb389e81f2c15d3aaff1cf9ff922483c811ce019
wangmr <wangminrui0804@gmail.com>
2022-04-13 17:53:01 +0800

    add base cmd: config,token,jobs,submit,stop

---
+++ mcccli/api.py
@@ -0,0 +1,149 @@
+import json
+import requests
+from .utils import to_str
+
+
+class API(object):
+
+    """API client for MCC Platform
+    """
+
+    def __init__(self, config):
+        self.config = config
+
+    def post_token(self, username, password, expiration=500000):
+        # TODO
+        # url = "{}/api/{}/token".format(self.config.api_uri, self.config.api_version)
+        # headers = {"Content-type": "application/json"}
+        # data = json.dumps({
+        #     "username": username,
+        #     "password": password,
+        #     "expiration": expiration
+        # })
+        # res = requests.post(url, headers=headers, data=data)
+
+        # if res.ok:
+        #     return to_str(res.content)
+        # else:
+        #     res.raise_for_status()
+        return json.dumps({
+            "token": "ABCDEFG"
+        })
+
+    def put_user(self):
+        pass
+
+    def delete_user(self):
+        """Admin only."""
+        pass
+
+    def put_user_username_virtualclusters(self):
+        """Admin only."""
+        pass
+
+    def get_jobs(self, username=""):
+        #TODO
+        # url = "{}/api/{}/jobs".format(self.config.api_uri, self.config.api_version)
+
+        # params = {} if not username else {"username": username}
+        # res = requests.get(url=url, params=params)
+
+        # if res.ok:
+        #     return to_str(res.content)
+        # else:
+        #     res.raise_for_status()
+        return json.dumps([
+            {
+                "name": "test1",
+                "username": "wangmr",
+                "virtualCluster": "default",
+                "createdTime": 5416578,
+                "retries": 1,
+                "state": "RUNNING",
+            },
+            {
+                "name": "test2",
+                "username": "wangmr",
+                "virtualCluster": "default",
+                "createdTime": 4178465,
+                "retries": 2,
+                "state": "SUCCEEDED",
+            },
+        ])
+
+    def get_user_username_jobs_jobname(self, username, jobname):
+        url = "{}/api/{}/user/{}/jobs/{}".format(
+            self.config.api_uri, self.config.api_version, username, jobname
+        )
+        res = requests.get(url)
+
+        if res.ok:
+            return to_str(res.content)
+        else:
+            res.raise_for_status()
+
+    def get_jobs_jobname_config(self, jobname):
+        pass
+
+    def get_user_username_jobs_jobname_ssh(self, username, jobname):
+        url = "{}/api/{}/user/{}/jobs/{}/ssh".format(
+            self.config.api_uri, self.config.api_version, username, jobname
+        )
+        res = requests.get(url)
+
+        if res.ok:
+            return to_str(res.content)
+        else:
+            res.raise_for_status()
+
+    def post_user_username_jobs(self, username, job_config_yaml):
+        #TODO
+        # url = "{}/api/{}/user/{}/jobs".format(self.config.api_uri, self.config.api_version, username)
+        # headers = self._headers_with_auth()
+
+        # res = requests.post(url, headers=headers, data=job_config_yaml)
+
+        # if res.ok:
+        #     return to_str(res.content)
+        # else:
+        #     res.raise_for_status()
+        print("Post job config yaml !!!")
+        # print(job_config_yaml)
+
+    def get_user_username_jobs_jobname_ssh(self, username, jobname):
+        url = "{}/api/{}/user/{}/jobs/{}/ssh".format(self.config.api_uri, self.config.api_version, username, jobname)
+        res = requests.get(url)
+
+        if res.ok:
+            return to_str(res.content)
+        else:
+            res.raise_for_status()
+
+    def put_user_username_jobs_jobname_executiontype(self, username, jobname, value):
+        #TODO
+        # url = "{}/api/{}/user/{}/jobs/{}/executionType".format(
+        #     self.config.api_uri, self.config.api_version, username, jobname
+        # )
+        # headers = self._headers_with_auth()
+        # data = json.dumps({"value": value})
+
+        # res = requests.put(url, headers=headers, data=data)
+        # if res.ok:
+        #     return to_str(res.content)
+        # else:
+        #     res.raise_for_status()
+        print("Put status: {}".format(value))
+
+    def get_virtualclusters(self):
+        pass
+
+    def get_virtualclusters_vcname(self, vcname):
+        pass
+
+    def _headers_with_auth(self):
+        self.config.load_access_token()
+        headers = {
+            "Authorization": "Bearer {}".format(self.config.access_token),
+            "Content-type": "application/json"
+        }
+        return headers
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='mcccli',
+    version="0.0.1",
+    description='Client for MCC Platform',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Minrui Wang',
+    url='',
+    author_email='wangminrui0804@gmail.com',
+    license='MIT',
+    install_requires=["requests",
+                      "pyyaml",
+                      "prompt_toolkit==1.0.9",
+                      "prettytable",
+                      "termcolor",
+                      "colorama",
+                      "click",
+                      ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': 'mcccli = mcccli.main:main'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
